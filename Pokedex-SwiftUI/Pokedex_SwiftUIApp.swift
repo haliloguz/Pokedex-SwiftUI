fdab8e2f67aa25ef79c1ab94400cad5955f96e20
Halil Oğuz <1oguzhalil@gmail.com>
2023-06-07 11:33:32 +0300

    Initial Commit

---
+++ Pokedex-SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex-SwiftUI
+//
+//  Created by Halil Oğuz on 7.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
